6275b77317197ca858483c7569ef3deb34ad242c
gringo lingo <gringolingoau@gmail.com>
2011-11-25 17:56:46 +1100

    my first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wordsofwisdom::Application.config.secret_token = '39acf20efae0a838e1b53a4433f71595b677891192457c850cab6f3ab030a768b1b44e04d7e87a6208a8129f0cdc19e40d36b335193c67f5a94db37dd49dfbd2'
